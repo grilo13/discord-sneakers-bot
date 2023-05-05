04a5e68847bd1e0856e7bd8d73a816dfb1565d51
pedro.grilo <pedro.sgrilo@decsis.pt>
2023-05-05 22:24:23 +0100

    Removed unused code

--- discord-bot.py
+++ discord-bot.py
@@ -41,22 +41,3 @@ async def on_ready():
 
 
 bot.run(DISCORD_TOKEN)
-
-"""class MyClient(discord.Client):
-    async def on_ready(self):
-        await check_sizes()
-        print(f'Logged on as {self.user}!')
-
-    async def on_message(self, message):
-        print(f'Message from {message.author}: {message.content}')
-        if message.content.startswith('hello'):
-            await message.channel.send('Hello!')
-
-        await check_sizes.start()
-
-
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = MyClient(intents=intents)
-client.run(DISCORD_TOKEN)"""
